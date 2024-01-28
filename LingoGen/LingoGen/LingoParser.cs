d061de37181bfb745bd39a36f0ad026ddf2abab2
Ruben <a.r.c.broere@students.uu.nl>
2024-01-28 20:46:28 +0100

    Revert "clear project"
    This reverts commit f289ea457a0e51455ffc358af48314bc688d8be8.

---
+++ LingoGen/LingoGen/LingoParser.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text.Json;
+using Microsoft.CodeAnalysis;
+
+namespace LingoGen;
+
+public class LingoParser
+{
+    private static LingoJson? ParseLingoJson(AdditionalText text)
+    {
+        // Check if the file name is the specific file that we expect.
+        if (Path.GetFileName(text.Path) != "lingo.json")
+            return null;
+
+        var json = text.GetText();
+
+        if (json is null)
+            return null;
+
+        var lingoJson = JsonSerializer.Deserialize<LingoJson>(json.ToString());
+
+        return lingoJson ?? null;
+    }
+}
